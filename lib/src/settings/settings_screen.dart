31051b42433d985941bc786eb8f96aee8f0389a7
Spenc-er <bxin1998@hotmail.com>
2023-08-10 15:47:11 +0800

    change settings font

--- lib/src/settings/settings_screen.dart
+++ lib/src/settings/settings_screen.dart
@@ -33,7 +33,7 @@ class SettingsScreen extends StatelessWidget {
               'Settings',
               textAlign: TextAlign.center,
               style: TextStyle(
-                fontFamily: 'Permanent Marker',
+                fontWeight: FontWeight.bold,
                 fontSize: 55,
                 height: 1,
               ),
@@ -137,7 +137,7 @@ class _NameChangeLine extends StatelessWidget {
           children: [
             Text(title,
                 style: const TextStyle(
-                  fontFamily: 'Permanent Marker',
+                  fontWeight: FontWeight.bold,
                   fontSize: 30,
                 )),
             const Spacer(),
@@ -146,7 +146,7 @@ class _NameChangeLine extends StatelessWidget {
               builder: (context, name, child) => Text(
                 '‘$name’',
                 style: const TextStyle(
-                  fontFamily: 'Permanent Marker',
+                  fontWeight: FontWeight.bold,
                   fontSize: 30,
                 ),
               ),
@@ -183,7 +183,7 @@ class _SettingsLine extends StatelessWidget {
                 maxLines: 1,
                 overflow: TextOverflow.ellipsis,
                 style: const TextStyle(
-                  fontFamily: 'Permanent Marker',
+                  fontWeight: FontWeight.bold,
                   fontSize: 30,
                 ),
               ),
